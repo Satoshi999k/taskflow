f9085a0148b5b762c0f3f432fd8e9611d12d7556
Marlo Bohol <mrlobhl14@gmail.com>
2026-08-08 11:13:42 +0800

    commit

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -3,7 +3,7 @@ import { supabase } from "./supabaseClient";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
-  const [showDashboard, setShowDashboard] = useState(true);
+  const [showDashboard, setShowDashboard] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
